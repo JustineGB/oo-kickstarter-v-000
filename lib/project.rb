1c36a4591b37e571810de3de50ab343f22fbec92
Justine Bricknell <justinegayton@gmail.com>
2018-07-26 20:14:53 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,6 +1,5 @@
 class Project   #Projects can HAVE MANY BACKERS. Backers can HAVE MANY Projects. So, it is a "HAS MANY" to "HAS MANY" relationship.
-  attr_accessor :title, :backers #:add_backer  #need to be able to set & get the title
-  #attr_writer :add_backer
+  attr_accessor :title, :backers #need to be able to set & get the title & set/get the backers
 
   def initialize(title)
     @title = title
@@ -12,6 +11,6 @@ class Project   #Projects can HAVE MANY BACKERS. Backers can HAVE MANY Projects.
     #how do we create the opposite relationship?
     #Need to add this instance of PROJECT! into the Backer's backed_projects array
     #(i.e. so he/she knows all of the project he./she is part of)
-    backer = backer.back_project
+    backer =  .back_project
   end
 end
