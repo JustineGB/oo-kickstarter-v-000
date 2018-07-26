a77db5b3fc4ecd9a97777f5b381b8d52ce1d1ad7
Justine Bricknell <justinegayton@gmail.com>
2018-07-26 18:21:17 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,14 @@
 class Backer
 
-  attr_accessor :backed_projects, :name, :project
+  attr_accessor :backed_projects, :name
+  attr_reader :project
 
   @backed_projects = []
 
   def initialize(name)
     @name = name
+    @backed_projects << self
+    @projects = []
   end
 
   def back_project(project)
