0415cac4ac804e9bc5b7d0a257291d4c14b139d5
Justine Bricknell <justinegayton@gmail.com>
2018-07-26 19:02:32 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -10,6 +10,7 @@ class Project
   def add_backer(backer)
     @backers << backer #stores this specific backer into the backer's array (owned by the Project Class)
     #Need to call the Backer Class in order to call their method @backed_projects and then save backer into it....
+    backer = Backer.back_project(backer)
   end
 end
   #(Backer as argument) and store in array! Need to refer to self and call Backer class in order to do so?
