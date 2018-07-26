294291b244db42e0020e25019691ecb329488976
Justine Bricknell <justinegayton@gmail.com>
2018-07-26 19:21:53 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,5 +1,6 @@
-class Backer
-  attr_accessor :backed_projects, :name, :project
+class Backer #has many recipricol relationship! w. has many you know you need to set up an empty array! (put into initialize method!)
+
+  attr_accessor :backed_projects, :name, :project #putting backed_projects here allows you to more easily read/write that array outside of this class!
 
   def initialize(name)
     @name = name
