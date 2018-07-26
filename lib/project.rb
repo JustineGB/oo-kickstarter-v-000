1e86de55b05c425f8ee23f0cc89277bfa4382c13
Justine Bricknell <justinegayton@gmail.com>
2018-07-26 18:56:51 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,6 @@
 class Project
   attr_accessor :title #need to be able to set & get the title
-  attr_reader :backers #just need to get (read) the backers (whihc are stored in an array). Project is to Backer as Song is to Artist or Dog to Owner. 
-
+  attr_reader :backers #just need to get (read) the backers (whihc are stored in an array). Project is to Backer as Song is to Artist or Dog to Owner?
 
   def initialize(title)
     @title = title
