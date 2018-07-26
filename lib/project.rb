3155bfb92531f974cc6840cffeda3267a20ab88e
Justine Bricknell <justinegayton@gmail.com>
2018-07-26 19:53:58 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -6,8 +6,8 @@ class Project   #Projects can HAVE MANY BACKERS. Backers can HAVE MANY Projects.
     @backers = []
   end
 
-  def add_backer(backer) #call self w/in this method? self to instance. then call Backer Class.back_projects method?
-    @backers << backer #stores this specific backer into the backer's array (owned by the Project Class)
-    backer.back_project(self)
+  def add_backer(backer) #pass in argument of a backer (person) and store this list of names into a backers array
+    @backers << backer #accessible to the Project class to know who its backers are on a specific project instance
+    #how do we create the oppostie relationship and allow the BACKER class to 
   end
 end
