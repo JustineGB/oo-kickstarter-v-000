c8df56d4a2ef31d62896397f7e3181d264c3b6bc
Justine Bricknell <justinegayton@gmail.com>
2018-07-26 19:48:05 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,8 +11,9 @@ class Backer #has many recipricol relationship! w. has many you know you need to
     #need to them set up opposite relationship so that PROJECT knows about the BACKER
     #Project is an object of the Project Class. Which means it has access to ANY method within its class. So it has access to its instance method
     #add_backer(backer) so we can use that method on this argument (project) bc (project) is an instance of the Project Class:
-    project.add_backer(project)
+    project.add_backer(self) #need to change this argument to self bc backer (argument from other class is not defined here in this class/method bc its just a local var)
+    #when you change to self it refers to the 
   end
 end
 
-#side note: if this was a has many/belongs to we could use project.backer.self (in the back_project method) 
+#side note: if this was a has many/belongs to we could use project.backer.self (in the back_project method)
