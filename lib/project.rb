7478936b7535c1723a1806e123cd60982ace5aaa
Justine Bricknell <justinegayton@gmail.com>
2018-07-26 19:45:20 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,5 +1,5 @@
 class Project   #Projects can HAVE MANY BACKERS. Backers can HAVE MANY Projects. So, it is a "HAS MANY" to "HAS MANY" relationship.
-  attr_accessor :title, :backers  #need to be able to set & get the title
+  attr_accessor :title, #:backers  #need to be able to set & get the title
 
   def initialize(title)
     @title = title
