7ec829a0089a3985c98167fe8f511d34f3c59bb6
Justine Bricknell <justinegayton@gmail.com>
2018-07-26 19:37:57 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,12 +1,11 @@
 class Backer #has many recipricol relationship! w. has many you know you need to set up an empty array! (put into initialize method!)
-  attr_accessor :backed_projects, :name, :project #putting backed_projects here allows you to more easily read/write that array outside of this class!
-
-  def initialize(name)
-    @name = name
-    @backed_projects = []
+  attr_accessor :backed_projects, :name #:project #putting backed_projects here allows you to more easily read/write that array outside of this class!
+  def initialize(name) #each instance is initialized with a name of a backer
+    @name = name #local name var that is passed into the meth is set to an instance var
+    @backed_projects = [] #empty array to save each instance
   end
 
-  def back_project(project)
-    @backed_projects << project
+  def back_project(project) #meth that takes in project argument
+    @backed_projects << project #save the local project var into the instance backed_projects array. so Backer class knows abot the project!
   end
 end
