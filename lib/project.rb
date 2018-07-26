ec1d4e62828b6651c807865bd7da093c42c56658
Justine Bricknell <justinegayton@gmail.com>
2018-07-26 18:53:54 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,5 +1,6 @@
 class Project
-  attr_accessor :title  #:backers
+  attr_accessor :title
+  attr_reader :backers #need to be able to set/get the title and set/get the backers (which is stored in an array) in order for this class to keep track of its backers?
 
 
   def initialize(title)
