97b292743c92341da82aaf4283698643a82e6d89
Justine Bricknell <justinegayton@gmail.com>
2018-07-26 18:44:56 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,13 +3,14 @@ class Backer
   attr_accessor :backed_projects, :name
   attr_reader :project #idk if I need this here at all, but if I do, it only needs to be a reader bc projects are created and initialized with a title in the Project Class
 
-  @backed_projects = []
 
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
 
   def back_project(project)
+
   end
 
 
